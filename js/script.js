acd60fedd71784278216b4f99590634181d1eeff
Max98002 <maksimka98002@gmail.com>
2020-01-28 15:32:47 +0200

    Написав скрипт для меню

---
+++ js/script.js
@@ -0,0 +1,12 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+  const menuButton = document.getElementById('menu-button'),
+      menu = document.getElementById('menu');
+
+  const openMenu = () => {
+    menu.classList.toggle('menu_active');
+  }
+
+  menuButton.addEventListener('click', openMenu);
+
+})
