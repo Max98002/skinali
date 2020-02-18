c48d24a2123309b57b0444619fbc9559e6e7fd13
Max98002 <maksimka98002@gmail.com>
2020-02-18 10:36:04 +0200

    add before

--- js/script.js
+++ js/script.js
@@ -9,7 +9,18 @@ document.addEventListener('DOMContentLoaded', () => {
 
   menuButton.addEventListener('click', openMenu);
 
-  
+  $(".before-after").twentytwenty({
+    before_label: 'До скиналі',
+    after_label: 'После скиналі'
+  });
 
+  $('.before-slider').slick({
+    draggable: false,
+    dots: true,
+    dotsClass: 'before-slider__dots',
+    swipe: false,
+    nextArrow: $('.arrow-left'),
+    prevArrow: $('.arrow-right')
+  });
 
 })
